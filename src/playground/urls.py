4fcecb4132dba3b8859a8b862b78cc546c2a30d5
Justin Rodney <justinandrewrodney@gmail.com>
2023-09-01 22:42:49 -0400

    Initial commit with items in playground/models

--- src/playground/urls.py
+++ src/playground/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='helloWorld/', view=views.helloWorld)
+    path(route='', view=views.main, name="Main"),
+    path(route='helloWorld/', view=views.helloWorld, name="Hello World"),
+    path(route='<int:id>/', view=views.index, name="Index")
 ]
--- src/playground/views.py
+++ src/playground/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
+def main(request):
+    print(request)
+    return HttpResponse('<h1>Main</h1>')
+
 def helloWorld(request):
     return render(request, 'helloWorld.html', {'name': "Justin"})
+
+def index(request, id):
+    print(request)
+    toDoList = ToDoList.objects.get(id=id)
+    return HttpResponse("<h1>%s</h1>" %toDoList.name)
